91a1baff0763c9b272aa5e502395d9d082e6cdb9
Cabanski, Tobias (tocab100) <tobias.cabanski@uni-duesseldorf.de>
2017-07-29 16:20:12 +0200

    Corrected simple seq2seq example: target input is not equal to target output since target input needs the GO symbol at the beginning and target output the EOS symbol at the end.

--- simple_seq2seq.py
+++ simple_seq2seq.py
@@ -24,31 +24,30 @@ def main():
     decoder_hidden_units = 50
 
     # Placeholders
-    encoder_inputs = tf.placeholder(shape=(None, None), dtype=tf.int32, name='encoder_inputs')
-    encoder_inputs_length = tf.placeholder(shape=(None,), dtype=tf.int32, name='encoder_inputs_length')
-    decoder_targets = tf.placeholder(shape=(None, None), dtype=tf.int32, name='decoder_targets')
+    tf_encoder_inputs = tf.placeholder(shape=(None, None), dtype=tf.int32, name='encoder_inputs')
+    tf_encoder_inputs_length = tf.placeholder(shape=(None,), dtype=tf.int32, name='encoder_inputs_length')
+    tf_decoder_targets_input = tf.placeholder(shape=(None, None), dtype=tf.int32, name='decoder_targets_input')
+    tf_decoder_targets_output = tf.placeholder(shape=(None, None), dtype=tf.int32, name='decoder_targets_output')
+    tf_decoder_targets_length = tf.placeholder(shape=(None,), dtype=tf.int32, name='decoder_target_length')
 
     # Variable embedding, error when placing on gpu (i don't know why)
     with tf.device('/cpu:0'):
         embeddings = tf.Variable(tf.random_uniform([vocab_size, input_embedding_size], -1.0, 1.0), dtype=tf.float32)
 
     # Embedding lookups for encoder and decoder
-    encoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, encoder_inputs)
-    decoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, decoder_targets)
+    encoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, tf_encoder_inputs)
+    decoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, tf_decoder_targets_input)
 
     # Encoder: Single LSTM Cell
     with tf.variable_scope("LSTM_Encoder") as vs:
         encoder_cell = LSTMCell(encoder_hidden_units)
         # encoder_cell2 = LSTMCell(encoder_hidden_units)
         (outputs, encoder_final_state) = tf.nn.dynamic_rnn(encoder_cell, inputs=encoder_inputs_embedded,
-                                                           sequence_length=encoder_inputs_length, dtype=tf.float32,
+                                                           sequence_length=tf_encoder_inputs_length, dtype=tf.float32,
                                                            time_major=True)
 
-    # Decoder length should be a litte longer because it has the EOS at the end
-    decoder_lengths = encoder_inputs_length + (target_seq_len - input_seq_len + 1)
-
     # Training Helper for Decoder Training
-    helper = tf.contrib.seq2seq.TrainingHelper(decoder_inputs_embedded, decoder_lengths, time_major=True)
+    helper = tf.contrib.seq2seq.TrainingHelper(decoder_inputs_embedded, tf_decoder_targets_length, time_major=True)
 
     # Projection Layer brings output from hidden layer count to count of unique classes (eg. 50 -> 10)
     projection_layer = layers_core.Dense(vocab_size, use_bias=False)
@@ -63,7 +62,7 @@ def main():
 
     # Cross entropy Loss
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
-        labels=tf.one_hot(decoder_targets, depth=vocab_size, dtype=tf.float32),
+        labels=tf.one_hot(tf_decoder_targets_output, depth=vocab_size, dtype=tf.float32),
         logits=final_outputs.rnn_output,
     )
 
@@ -81,14 +80,19 @@ def main():
     try:
         for batch in range(max_batches):
             # Get batch
-            encoder_input, encoder_inputs_lengths, decoder_target = createBatch(batch_size, input_seq_len, vocab_size,
-                                                                                target_seq_len)
+            encoder_input, \
+            encoder_inputs_lengths, \
+            decoder_target_input, \
+            decoder_targets_output = createBatch(batch_size, input_seq_len, vocab_size, target_seq_len)
 
             # Feed dict
             fd = {
-                encoder_inputs: encoder_input,
-                encoder_inputs_length: encoder_inputs_lengths,
-                decoder_targets: decoder_target,
+                tf_encoder_inputs: encoder_input,
+                tf_encoder_inputs_length: encoder_inputs_lengths,
+                tf_decoder_targets_input: decoder_target_input,
+                tf_decoder_targets_output: decoder_targets_output,
+                # target len equal in every output in this example
+                tf_decoder_targets_length: np.array([target_seq_len]*batch_size)
             }
 
             # train net
@@ -113,28 +117,41 @@ def main():
 def createBatch(batch_size, seq_len, vocab_size, target_len, time_major=True):
     # Symbol for PAD (padding of a sequence) and end of sentence (EOS)
     PAD = 0
-    EOS = 1
+    GO = 1
+    EOS = 2
 
     # First define length of every sequence
     lengths = np.random.randint(1, seq_len, batch_size)
 
-    # Create encoder input, begin at 2 because 0 and 1 are reserved for PAD and EOS
+    # Create encoder input, begin at 3 because: 0: PAD, 1: GO, 2: EOS
+    # Normally we also need a UNK (unknown) Symbol, but for this example we leave this out
     encoder_inputs = []
-    decoder_targets = []
+    decoder_targets_input = []
+    decoder_targets_output = []
+
     for i in range(batch_size):
         rand_seq = np.random.randint(2, vocab_size, lengths[i]).tolist()
+
+        # Data for Encoder only needs PAD Symbol
         encoder_inputs.append(rand_seq + [PAD] * (seq_len - len(rand_seq)))
-        decoder_targets.append(rand_seq + [EOS] + [0] * (target_len - len(rand_seq) - 1))
+
+        # Decoder target inputs need the GO Symbol at the beginning of the sequence
+        decoder_targets_input.append([GO] + rand_seq + [PAD] * (target_len - len(rand_seq)-1))
+
+        # Decoder target outputs need the EOS Symbol at the end of the sequence
+        decoder_targets_output.append(rand_seq + [EOS] + [PAD] * (target_len - len(rand_seq)-1))
 
     # to np array
     encoder_inputs = np.array(encoder_inputs)
-    decoder_targets = np.array(decoder_targets)
+    decoder_targets_input = np.array(decoder_targets_input)
+    decoder_targets_output = np.array(decoder_targets_output)
 
     if time_major:
         encoder_inputs = encoder_inputs.T
-        decoder_targets = decoder_targets.T
+        decoder_targets_input = decoder_targets_input.T
+        decoder_targets_output = decoder_targets_output.T
 
-    return encoder_inputs, lengths, decoder_targets
+    return encoder_inputs, lengths, decoder_targets_input, decoder_targets_output
 
 
 if __name__ == "__main__":
